c0f7969f1de21c1a4c0218b75daff711828b58f8
Abhignakp <abhignakp05@gmail.com>
2025-01-23 12:54:32 +0530

    Update empty_array.js

--- advance js/empty_array.js
+++ advance js/empty_array.js
@@ -1,4 +1,4 @@
 const isArrayEmpty = (arr) => Array.isArray(arr) && arr.length === 0;
 
-console.log(isArrayEmpty([])); // Output: true
-console.log(isArrayEmpty([1, 2, 3])); // Output: false
+console.log(isArrayEmpty([])); 
+console.log(isArrayEmpty([1, 2, 3])); 
