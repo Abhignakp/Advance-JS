9cc2f1df8f783b8da949568a68320c80d8f0560d
Abhignakp <abhignakp05@gmail.com>
2025-01-23 12:55:15 +0530

    Update shallow_copy&deep_copy.js

--- advance js/shallow_copy&deep_copy.js
+++ advance js/shallow_copy&deep_copy.js
@@ -2,7 +2,7 @@ const obj = { name: "John", details: { age: 30, city: "New York" } };
 
 // Shallow Copy using Object.assign
 const shallowCopy = Object.assign({}, obj);
-shallowCopy.details.age = 40; // Changes the original object too
+shallowCopy.details.age = 40; 
 console.log(obj.details.age); // Output: 40
 
 // Deep Copy using structuredClone (modern approach)
